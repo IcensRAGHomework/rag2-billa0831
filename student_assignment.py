df5d254bfa7badca9c48673627171cc6f543870c
billa0831 <billa0831@gmail.com>
2025-02-18 09:43:09 +0800

    Add files via upload

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,24 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    # pages = []
+    # pages = loader.load_and_split()
+    # print(pages)
+    # docs = ""
+    # for item in pages:
+    #     print(item.metadata)
+    #     # docs += item.page_content
+    #     # print(docs)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separator="\n")
+    chunks = text_splitter.split_documents(documents=documents)
+    last_chunk = chunks[-1]
+    print(last_chunk)
+    return last_chunk
 
 def hw02_2(q2_pdf):
     pass
+
+print("star hw2")
+hw02_1(q1_pdf)
